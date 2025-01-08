a274fcb9dcc618eed4b088d8d711f66d70010373
Yadhavaramanan C <144922779+Yadhavaramanan@users.noreply.github.com>
2025-01-08 12:04:36 +0530

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -16,6 +16,24 @@ public class Main
 		map1.get(2.0f).put("3","dog");
 		map1.get(2.0f).put("4","cat");
 		System.out.println(map1);
-		
-	}
+        
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"Muthu");
+        map.put(2,"Kumar");
+        map.put(3,"ram");
+        map.put(4,"Vel");
+        
+        for(Map.Entry<Integer,String> e:map.entrySet() ){
+            System.out.println(e);
+        }
+        System.out.println("--------------------------------------------------");
+        for(int e1:map.keySet()){
+            System.out.println(e1);
+        }
+        System.out.println("-".repeat(50));
+        for(String e2:map.values()){
+            System.out.println(e2);
+        }
+        System.out.println("-".repeat(50));
+    }
 }
