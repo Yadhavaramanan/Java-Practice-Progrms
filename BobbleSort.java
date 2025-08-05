365be9adaf81accaa2d12dcec6a9bfee71c81957
Yadhavaramanan C <144922779+Yadhavaramanan@users.noreply.github.com>
2025-08-05 22:23:13 +0530

    Update BobbleSort.java

--- BobbleSort.java
+++ BobbleSort.java
@@ -12,8 +12,8 @@ public class Main
 	}
 	
 	public static void BubbleSort(int[] arr, int n){
-	    for(int i=0;i<n;i++){
-	        for(int j=0;j<n-1;j++){
+	    for(int i=0;i<n-1;i++){
+	        for(int j=0;j<n-i-1;j++){
 	            if(arr[j]>arr[j+1]){
 	                int temp = arr[j+1];
 	                arr[j+1] = arr[j];
