b1f65631d88142aea18e38fb88825f0004c7e9b9
Yadhavaramanan C <144922779+Yadhavaramanan@users.noreply.github.com>
2025-08-06 12:51:36 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,64 +1,124 @@
 import java.util.*;
-
 class Node{
     int data;
-    Node link;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+class LinkedList{
+    Node head;
+    int size = 0;
+    void AddAtFront(int n){
+        Node newNode = new Node(n);
+        if(head == null){
+            head = newNode;
+            size++;
+        }else{
+            newNode.next = head;
+            head = newNode;
+            size++;
+        }
+    }
     
-    Node(int val){
-        data = val;
-        link = null; 
+    void AddAtEnd(int n){
+        Node temp = head;
+        Node newNode = new Node(n);
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        size++;
+    }
+    
+    void AddAtMiddle(int index, int n){
+        Node newNode = new Node(n);
+        if(index > size){
+            System.out.println("Index is out of found");
+        }else if(index == 0){
+            AddAtFront(n);
+        }
+        else{
+            Node temp = head;
+            int i = 0;
+            while(i < index-2){
+                temp = temp.next;
+                i++;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
+        }
+    }
+    
+    void DeleteAtFront(){
+        if(head == null){
+            System.out.println("List is empty.");
+        }else{
+            head = head.next;
+            size--;
+        }
+    }
+    
+    void DeleteAtEnd(){
+        Node temp = head;
+        while(temp.next.next != null){
+            temp = temp.next;
+        }
+        temp.next = null;
+        size--;
+    }
+    
+    void DeleteAtMiddle(int index){
+        if(index > size){
+            System.out.println("Index is out of found");
+        }else{
+            Node temp = head;
+            int i = 0;
+            while(i < index-2){
+                temp = temp.next;
+                i++;
+            }
+            temp.next = temp.next.next;
+            size--;
+        }
+    }
+    
+    void print(){
+        if(head == null){
+            System.out.print("List is empty.");
+        }else{
+            Node temp = head;
+            while(temp != null){
+                System.out.print(temp.data +"-->");
+                temp= temp.next;
+            }
+ 
+            System.out.println("null");
+        }
     }
 }
 public class Main
 {
 	public static void main(String[] args) {
-		Node n1 = new Node(10);
-		Node n2 = new Node(20);
-		Node n3 = new Node(30);
-		Node n4 = new Node(40);
-		
-		System.out.println("n1--"+n1+" :"+n1.data+"-->"+n1.link);
-		System.out.println("n2--"+n2+" :"+n2.data+"-->"+n2.link);
-		System.out.println("n3--"+n3+" :"+n3.data+"-->"+n3.link);
-		System.out.println("n4--"+n4+" :"+n4.data+"-->"+n4.link);
-		System.out.println("-".repeat(50));
-		n1.link = n2;
-		n2.link = n3;
-		n3.link = n4;
-		
-		System.out.println("n1--"+n1+" :"+n1.data+"-->"+n1.link);
-		System.out.println("n2--"+n2+" :"+n2.data+"-->"+n2.link);
-		System.out.println("n3--"+n3+" :"+n3.data+"-->"+n3.link);
-		System.out.println("n4--"+n4+" :"+n4.data+"-->"+n4.link);
-		System.out.println("-".repeat(50));
-		System.out.println(n1.data+"-->"+n1.link.data+"-->"+n1.link.link.data+"-->"+n1.link.link.link.data+"-->"+n1.link.link.link.link);
-	
-	    System.out.println("-".repeat(50));
-	    
-	    Node tempNode = n1;
-	    System.out.println(tempNode.data);
-	    
-	    tempNode = tempNode.link;
-	    System.out.println(tempNode.data);
-	    
-	    tempNode = tempNode.link;
-	    System.out.println(tempNode.data);
-	    
-	    tempNode = tempNode.link;
-	    System.out.println(tempNode.data);
-	    
-	    tempNode = tempNode.link;
-	    System.out.println(tempNode);
-	    
-	    System.out.println("-".repeat(50));
-	    tempNode = n1;
-	    while(tempNode !=null){
-	        System.out.print(tempNode.data+"-->");
-	        tempNode = tempNode.link;
-	    }
-	    System.out.print(tempNode+"\n");
-	    
-	    System.out.println("-".repeat(50));
+		LinkedList ll = new LinkedList();
+		ll.AddAtFront(10);
+		ll.AddAtEnd(20);
+		ll.AddAtEnd(30);
+		ll.AddAtEnd(50);
+		ll.AddAtEnd(70);
+		ll.AddAtEnd(80);
+		ll.print();
+		ll.AddAtMiddle(4,40);
+		ll.print();
+		ll.AddAtMiddle(6,60);
+		ll.print();
+		ll.DeleteAtFront();
+		ll.print();
+		ll.DeleteAtEnd();
+		ll.print();
+		ll.DeleteAtMiddle(5);
+		ll.print();
 	}
-	
 }
