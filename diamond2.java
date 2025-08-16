fa2698d6555a1ef13cbd6bc2d8c89fdd855fb80e
Yadhavaramanan C <144922779+Yadhavaramanan@users.noreply.github.com>
2025-08-16 20:35:12 +0530

    Create diamond2.java

---
+++ diamond2.java
@@ -0,0 +1,46 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Patterndiamond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+    //upper
+		for(int i=1;i<=n;i++) {
+			for(int j=i;j<n;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+		//lower
+		for(int i=n-1;i>=1;i--) {
+			for(int j=i;j<n;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+  }
+}
+
+Input:
+4
+Output:
+   * 
+  * * 
+ * * * 
+* * * * 
+ * * * 
+  * * 
+   * 
+
